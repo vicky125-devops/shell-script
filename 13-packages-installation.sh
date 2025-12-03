7021587d4c7f80019f2a23294bfb5ee19635505f
pavankalyan <pavankalaynr125@gamil.com>
2025-12-03 14:17:43 +0530

    seventh commit

--- 13-packages-installation.sh
+++ 13-packages-installation.sh
@@ -32,7 +32,7 @@ for i in $@
 do
   echo "package to install: $i"
   apt list --installed $i &>>$LOGFILE
-  if [ $? -eq 0 ]
+  if dpkg-query -W --showformat='${Status}\n' "$i" 2>/dev/null | grep "install ok installed" &>>$LOGFILE;
   then
       echo -e "$i is already installed...$Y SKIPPING $N"
   else
